a288489c8845cd9c638ddccaf7d5fe8ba5f12728
GerardoLorenzo <glorenzo68@yahoo.com>
2020-07-20 16:42:46 -0300

    first commit, added guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,31 @@
+function getcountoffiles {  				## This function return the number of files 
+	let count=$(ls -l | egrep '^-' | wc -l)		## in the current folder and saves it in the
+}							## count variable
+## Start Script
+clear
+## Variable initialization
+let count=0      ## Stores the number of files in the current folder
+let number=-1	 ## Stores the number entered by the user
+let intents=0	 ## Stores the number of user attemps
+getcountoffiles  ## After execute the function, the number of files are stored in count
+
+echo "Hello, try to guess how many files are in the current folder..."
+echo -n "Input a number: "
+read number
+let intents=intents+1
+
+until [ $count = $number ]
+do
+	if [ $count -gt $number ]
+	then
+		echo "There are more files, try a larger number..."
+	else
+		echo "There are fewer files, try a smaller number..."
+	fi
+	echo -n "Input a number: "
+	read number
+	let intents=intents+1
+done
+
+echo "Pertect! You got it in "$intents" tries, there are "$count" files in this directory."
+echo ""
