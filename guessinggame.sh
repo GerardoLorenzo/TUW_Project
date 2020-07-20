d6fed144ab7752f45255a114ff1aa3db29600330
GerardoLorenzo <glorenzo68@yahoo.com>
2020-07-20 18:11:50 -0300

    guessinggame.sd modified

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,22 +1,24 @@
-function getcountoffiles {  				## This function return the number of files 
-	let count=$(ls -l | egrep '^-' | wc -l)		## in the current folder and saves it in the
-}							## count variable
+function getcountoffiles {  			## This is the required function, return the number of files 
+	let count=$(ls -l | egrep '^-' | wc -l)	## in the current folder and saves it in the count variable.
+}						## Folders are not included, only files
+
 ## Start Script
 clear
 ## Variable initialization
 let count=0      ## Stores the number of files in the current folder
 let number=-1	 ## Stores the number entered by the user
 let intents=0	 ## Stores the number of user attemps
-getcountoffiles  ## After execute the function, the number of files are stored in count
+
+getcountoffiles  ## Run function and the number of files are stored in count
 
 echo "Hello, try to guess how many files are in the current folder..."
 echo -n "Input a number: "
 read number
 let intents=intents+1
 
-until [ $count = $number ]
+until [ $count = $number ]  		## This is the required loop
 do
-	if [ $count -gt $number ]
+	if [ $count -gt $number ]	## This is the required if statement
 	then
 		echo "There are more files, try a larger number..."
 	else
